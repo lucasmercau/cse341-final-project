d2da50586a15879ac7e28b69ddc95536a53410b3
Abdul Mahl <mdu_abdul@icloud.com>
2024-02-07 21:47:48 +0200

    .

--- routes/moviesRoute.js
+++ routes/moviesRoute.js
@@ -1,5 +1,5 @@
 const movieRouter = require("express").Router();
-const controller = require("../controllers/moviesFunction");
+const controller = require("../controllers/moviesFunctions");
 
 movieRouter.get("/", controller.getAll);
 movieRouter.get("/:id", controller.getById);
