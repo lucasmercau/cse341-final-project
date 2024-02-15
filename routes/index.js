ff55838fb28d0da01aab8bd26ae38580bb3446ee
Abdul Mahl <mdu_abdul@icloud.com>
2024-02-15 15:15:19 +0200

    .

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,25 @@
 const router = require("express").Router();
+const passport = require("passport");
+const cors = require("cors");
+
+// Use CORS middleware
+router.use(cors());
 
 router.use("/", require("./swagger"));
-
 router.use("/movies/", require("./moviesRoute"));
-
 router.use("/cast/", require("./castsRoute"));
 
-// This is our oauth authentication route...
+// Define authentication routes
 router.use("/auth/google", require("../controllers/google-auth"));
 router.use("/auth/github", require("../controllers/github-auth"));
 router.use("/auth/facebook", require("../controllers/facebook-auth"));
 
-// This is the root path handler
-router.get("/", (req, res) => {
-  res.render("auth");
-});
+// Root path handler for authentication
+router.get("/", 
+  passport.authenticate(["google", "github", "facebook"], { failureRedirect: "/" }), 
+  (req, res) => {
+    res.redirect("/success"); // Redirect to the dashboard upon successful authentication
+  }
+);
 
 module.exports = router;
