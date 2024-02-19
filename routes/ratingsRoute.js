84a0dad63f2358135622885616644ebb2b088a46
Abdul Mahl <mdu_abdul@icloud.com>
2024-02-19 20:56:17 +0200

    .

--- routes/ratingsRoute.js
+++ routes/ratingsRoute.js
@@ -1,10 +1,11 @@
 const ratingRouter = require("express").Router();
 const controller = require("../controllers/ratingsFunction");
+const { isAuthenticated } = require("../models/authenticate");
 
 ratingRouter.get("/", controller.getAllRatings);
 ratingRouter.get("/:id", controller.getRatingById);
-ratingRouter.post("/", controller.createRating);
-ratingRouter.put("/:id", controller.updateRating);
-ratingRouter.delete("/:id", controller.deleteRating);
+ratingRouter.post("/", isAuthenticated, controller.createRating);
+ratingRouter.put("/:id", isAuthenticated, controller.updateRating);
+ratingRouter.delete("/:id", isAuthenticated, controller.deleteRating);
 
 module.exports = ratingRouter;
