c4ccf206f654002f32cf208127d8436031af1c23
Abdul Mahl <mdu_abdul@icloud.com>
2024-02-19 19:35:44 +0200

    .

--- routes/facebookRoute.js
+++ routes/facebookRoute.js
@@ -3,8 +3,6 @@ const facebookRouter = express.Router();
 const controller = require("../controllers/usersFunction");
 const { isAuthenticated } = require("../models/authenticate");
 
-
-
 facebookRouter.get("/", controller.getFacebookUsers);
 facebookRouter.get("/:id", controller.getFacebookUserById);
 facebookRouter.put("/:id", isAuthenticated, controller.updateFacbookUser);
