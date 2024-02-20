f9254e58cdde7b47574d7f384f1cf68bdf65663c
Abdul Mahl <mdu_abdul@icloud.com>
2024-02-20 11:59:51 +0200

    .

--- models/authenticate.js
+++ models/authenticate.js
@@ -1,6 +1,6 @@
 const isAuthenticated = (req, res, next) => {
   if (req.session.user === undefined) {
-    res.status(401).json({ message: "You are not authorized" });
+    res.status(401).json({ message: "You are not authorized for this action" });
   } else {
     next();
   }
